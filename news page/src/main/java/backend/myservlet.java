7dd38d400ffb010d3f4f995965260c8ddc47e3d9
Aashik Ali <84761597+aashiqlove@users.noreply.github.com>
2023-11-28 15:13:43 +0530

    Add files via upload

---
+++ news page/src/main/java/backend/myservlet.java
@@ -0,0 +1,54 @@
+package backend;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class myservlet
+ */
+@WebServlet("/myservlet")
+public class myservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public myservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+            String value = request.getParameter("value");
+            // Use the value parameter as needed (e.g., process it or send a response)
+            System.out.println("Received value from newsbox: " + value);
+            // You can send a response back if needed
+            response.getWriter().write("Received value from newsbox: " + value);
+        
+            
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
